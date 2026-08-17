8c0d13b183b8632f7566bbe5bafc6e45fa9f3e7a
Fabi-22 <Fvalenciab@eafit.edu.co>
2026-08-16 22:00:21 -0500

    chore(admin): registrar modelos en el panel de administración

--- Volt/admin.py
+++ Volt/admin.py
@@ -1,3 +1,44 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Address, Cart, CartItem, Category, Order, OrderItem, Product
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    search_fields = ('name',)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'category', 'price', 'stock')
+    list_filter = ('category',)
+    search_fields = ('name',)
+
+
+@admin.register(Address)
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'city', 'is_default')
+
+
+class CartItemInline(admin.TabularInline):
+    model = CartItem
+    extra = 0
+
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'created_at')
+    inlines = [CartItemInline]
+
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 0
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'status', 'total', 'created_at')
+    list_filter = ('status',)
+    inlines = [OrderItemInline]
